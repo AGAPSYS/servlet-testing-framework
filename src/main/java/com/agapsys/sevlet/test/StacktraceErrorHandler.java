5b22fadb23f86e9ebf76315ac26fcf1c57ad4a53
Leandro Oliveira <leandro@agapsys.com>
2015-09-17 12:11:17 -0300

    Using baseRequest instead

--- src/main/java/com/agapsys/sevlet/test/StacktraceErrorHandler.java
+++ src/main/java/com/agapsys/sevlet/test/StacktraceErrorHandler.java
@@ -28,8 +28,8 @@ public class StacktraceErrorHandler extends ErrorHandler {
 	// CLASS SCOPE =============================================================
 	private static final String ATTR_EXCEPTION = "javax.servlet.error.exception";
 	
-	private static String getStackTrace(HttpServletRequest req) {
-		Throwable throwable = (Throwable) req.getAttribute(ATTR_EXCEPTION);
+	private static String getStackTrace(Request baseRequest) {
+		Throwable throwable = (Throwable) baseRequest.getAttribute(ATTR_EXCEPTION);
 		StringWriter stringWriter = new StringWriter();
 		throwable.printStackTrace(new PrintWriter(stringWriter));
 		return stringWriter.toString();
@@ -39,7 +39,7 @@ public class StacktraceErrorHandler extends ErrorHandler {
 	// INSTANCE SCOPE ==========================================================
 	@Override
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.getWriter().print(getStackTrace(request));
+		response.getWriter().print(getStackTrace(baseRequest));
 	}
 	// =========================================================================
 }
--- src/test/java/com/agapsys/sevlet/test/ErrorHandlerTest.java
+++ src/test/java/com/agapsys/sevlet/test/ErrorHandlerTest.java
@@ -22,7 +22,6 @@ import org.junit.After;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import org.junit.Ignore;
 
 public class ErrorHandlerTest {
 	private ServletContainer sc;
